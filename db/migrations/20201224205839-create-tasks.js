60ce9617dcb8c86fe5ce4eee726677202d6aea9f
Brian Izaki <bkizaki@outlook.com>
2020-12-28 14:11:03 -0300

    fix: atributo id duplicado

--- db/migrations/20201224205839-create-tasks.js
+++ db/migrations/20201224205839-create-tasks.js
@@ -3,13 +3,9 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Tasks', {
       id: {
-        allowNull: false,
-        autoIncrement: true,
+        type: Sequelize.INTEGER,
         primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      id: {
-        type: Sequelize.INTEGER
+        autoIncrement: true
       },
       tittle: {
         type: Sequelize.STRING
