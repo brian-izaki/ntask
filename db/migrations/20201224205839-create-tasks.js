e2ddf7b7cbe77403cdab6719a7e29a0273a1f17a
Brian Izaki <bkizaki@outlook.com>
2020-12-28 22:06:51 -0300

    refactor: foreign key

--- db/migrations/20201224205839-create-tasks.js
+++ db/migrations/20201224205839-create-tasks.js
@@ -7,6 +7,13 @@ module.exports = {
         primaryKey: true,
         autoIncrement: true
       },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
       tittle: {
         type: Sequelize.STRING
       },
@@ -22,8 +29,19 @@ module.exports = {
         type: Sequelize.DATE
       }
     });
+
+    // await queryInterface.addConstraint('Tasks', {
+    //   fields: ['user_id'],
+    //   type: 'foreign key',
+    //   name: 'FK_USER__TASKS',
+    //   references: {
+    //     table: 'Users',
+    //     field: 'id'
+    //   }
+    // })
   },
   down: async (queryInterface, Sequelize) => {
+    // await queryInterface.removeConstraint('Tasks', 'FK_USER__TASKS')
     await queryInterface.dropTable('Tasks');
   }
 };
