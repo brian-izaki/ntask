ab6fd1a2141950e73fa6268be7f65c5f53d26d83
Brian Izaki <bkizaki@outlook.com>
2021-01-13 16:49:06 -0300

    feat: config para implementar o https

--- libs/boot.js
+++ libs/boot.js
@@ -1,6 +1,13 @@
+const https = require("https"),
+  fs = require("fs");
+
 module.exports = (app) => {
   if (process.env.NODE_ENV !== "test") {
-    app.listen(app.get("port"), () => {
+    const credentials = {
+      key: fs.readFileSync("server.key", "utf-8"),
+      cert: fs.readFileSync("server.cert", "utf-8"),
+    };
+    https.createServer(credentials, app).listen(app.get("port"), () => {
       console.log(`iniciado ${app.get("port")}`);
     });
   }
